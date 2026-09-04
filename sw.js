37f7d53411a3e6eb0278b1144fecf35536709474
孙特 <7551866@gmail.com>
2026-09-04 16:37:20 +0800

    Refresh PWA cache for sync config

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "java-notes-v2";
+const CACHE_NAME = "java-notes-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png", "./sync-config.js"];
 
 self.addEventListener("install", event => {
